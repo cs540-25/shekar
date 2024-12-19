e48a9e04e4ccc0f1869020a16505a1110c8a11d0
Ahmad Amirivojdan <amirivojdan@gmail.com>
2024-12-19 16:04:21 -0500

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,14 +1,16 @@
 import pytest
 from shekar.utils import is_informal
 
+
 def test_is_informal():
     input_text = "میخوام برم خونه، تو نمیای؟"
-    expected_output = (True, 4)
+    expected_output = True
     assert is_informal(input_text) == expected_output
 
     input_text = "دیگه چه خبر؟"
-    expected_output = (True, 1)
+    expected_output = True
     assert is_informal(input_text) == expected_output
+
 
 if __name__ == "__main__":
     pytest.main()
